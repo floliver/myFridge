00f85df4fda6df865f6763bf55d73d75e118ced3
unknown <foliver@LFR410175.corp.capgemini.com>
2015-03-04 17:00:30 +0100

    add first sources

---
+++ src/main/java/com/myfridge/myfridge/models/Fridge.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myfridge.myfridge.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author foliver
+ */
+public class Fridge {
+    private List<String> listItems = new ArrayList<>();
+    
+    
+}
---
+++ src/main/java/com/myfridge/myfridge/models/Item.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myfridge.myfridge.models;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author foliver
+ */
+public class Item {
+    private LocalDate expirationDate = LocalDate.now();
+    private String description = new String();
+    
+    
+    
+}
